c163830dbeae0f0dba61eca342b27d51529f1539
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-01-07 11:20:11 -0500

    added the abstract modal

--- js/addContent.js
+++ js/addContent.js
@@ -1,12 +1,13 @@
 //This file maps the information in the content.json file to the specific elemenet
 
 $(document).ready(function () {
-    const title = $('.list-talk')
+    const title = $('.list-talk-title')
     const speaker = $('.list-speaker')
     const time = $('.list-time')
     const date = $('.list-date')
     const location = $('.list-address')
     const meetupUrl = $('.attend-button')
+    const abstract = $('.abstract')
 
     $.getJSON('content.json', function (content) {
       title.append(content.nextevent.title)
@@ -15,5 +16,6 @@ $(document).ready(function () {
       date.append(content.nextevent.date)
       location.append(content.nextevent.location)
       meetupUrl.attr("href", content.nextevent.meetupUrl)
+      abstract.append(content.nextevent.abstract)
     })
   })
