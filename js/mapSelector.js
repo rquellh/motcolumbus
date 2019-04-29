e37f882e60e440648f2b307212600aaf49a0b1c2
rquellh <rquellh@gmail.com>
2019-04-29 08:30:47 -0400

    Update mapSelector.js

--- js/mapSelector.js
+++ js/mapSelector.js
@@ -3,7 +3,7 @@ function mapSelector() {
         ((navigator.platform.indexOf("iPhone") != -1) ||
         (navigator.platform.indexOf("iPad") != -1) ||
         (navigator.platform.indexOf("iPod") != -1))
-        window.open("maps://www.google.com/maps/place/460+Polaris+Pkwy+%23150,+Westerville,+OH+43082/@40.147438,-82.9472119,17z/data=!3m1!4b1!4m5!3m4!1s0x8838f5b17b939639:0xa48a48432cd880fb!8m2!3d40.147438!4d-82.9450232");
+        window.open("maps://www.google.com/maps/place/570+Polaris+Pkwy+%23250,+Westerville,+OH+43082/@40.1471571,-82.9503079,17.5z/data=!4m5!3m4!1s0x8838f5b2c2b60dbd:0x996dea1231e82fb7!8m2!3d40.1470574!4d-82.9488954");
     else /* else use Google */
-        window.open("https://www.google.com/maps/place/460+Polaris+Pkwy+%23150,+Westerville,+OH+43082/@40.147438,-82.9472119,17z/data=!3m1!4b1!4m5!3m4!1s0x8838f5b17b939639:0xa48a48432cd880fb!8m2!3d40.147438!4d-82.9450232");
+        window.open("https://www.google.com/maps/place/570+Polaris+Pkwy+%23250,+Westerville,+OH+43082/@40.1471571,-82.9503079,17.5z/data=!4m5!3m4!1s0x8838f5b2c2b60dbd:0x996dea1231e82fb7!8m2!3d40.1470574!4d-82.9488954");
 }
